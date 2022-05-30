374475e3b9a96d37d4e12a79341abd7143eeda16
Fábio Salata <fsalata@gmail.com>
2022-05-29 22:53:22 -0300

    chapter 7

--- 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 3.xcplaygroundpage/Contents.swift
+++ 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 3.xcplaygroundpage/Contents.swift
@@ -3,15 +3,57 @@
 /*:
  [Previous Challenge](@previous)
  ## Challenge 3: Reverse a linked list
-
+ 
  Create a function that reverses a linked list. You do this by manipulating the nodes so that they’re linked in the other direction.
  */
 extension LinkedList {
     
-  mutating func reverse() {
+    mutating func reverse() {
+        var tempList = LinkedList<Value>()
+        
+        for value in self {
+            tempList.push(value)
+        }
+        
+        head = tempList.head
+    }
     
-  }
+    mutating func reverse2() {
+        tail = head
+        var prev = head
+        var current = head?.next
+        prev?.next = nil
+        
+        while current != nil {
+            let next = current?.next
+            current?.next = prev
+            prev = current
+            current = next
+        }
+        
+        head = prev
+    }
 }
 
+example(of: "reversing a list") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print("Original list: \(list)")
+    list.reverse()
+    print("Reversed list: \(list)")
+}
 
+example(of: "reversing a list 2 ") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print("Original list: \(list)")
+    list.reverse2()
+    print("Reversed list: \(list)")
+}
 //: [Next Challenge](@next)
--- 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 5.xcplaygroundpage/Contents.swift
+++ 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 5.xcplaygroundpage/Contents.swift
@@ -10,6 +10,37 @@
 extension LinkedList where Value: Equatable {
   
   mutating func removeAll(_ value: Value) {
-
+      while let head = head, head.value == value {
+          self.head = head.next
+      }
+      
+      var prev = head
+      var current = head?.next
+      
+      while let currentNode = current {
+          guard currentNode.value != value else {
+              prev?.next = currentNode.next
+              current = prev?.next
+              continue
+          }
+          
+          prev = current
+          current = current?.next
+      }
+      
+      tail = prev
   }
+}
+
+example(of: "deleting duplicate nodes") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(2)
+    list.push(1)
+    list.push(1)
+    list.push(3)
+    
+    list.removeAll(3)
+    print(list)
 }
